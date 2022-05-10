039a093e2e4021d7985ee41e727fbd948ab9d5e1
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-05-10 06:27:37 -0700

    Update csvdatahandling.py

--- csvdatahandling.py
+++ csvdatahandling.py
@@ -8,3 +8,11 @@ data_d = pd.read_csv('ex2.csv',header=None)
 print(data_d)
 data_d = pd.read_csv('ex2.csv',header=None, names=['N1','N2','N3','Prog_name'])
 data_d
+#assign row and columns label
+names_index =['N1','N2','N3','Prog_name']
+data_c = pd.read_csv('ex2.csv',header=None, names=names_index, index_col="Prog_name")
+data_c
+#lets see data type of our file
+!type hindex.csv
+data_hind = pd.read_csv('hindex.csv',index_col =['cat','type'])
+data_hind
