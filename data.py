a2d1a3a73b26b49c9f64dbce15ad7cf56da68487
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-05-12 06:50:02 -0700

    Update data.py

--- data.py
+++ data.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import numpy as np
+pd.DataFrame.to_csv?
+data = pd.read_csv('missing_file.csv')
+data
+data.to_csv('new_missing.csv')
+data.to_excel('new_missingexcel.xlsx',encoding = 'utf-8')
+data.to_csv('new_sep.csv',sep ='|')
+!type new_sep.csv
