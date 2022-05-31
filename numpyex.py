53f42b4124473a90775a53497da9246daca7296f
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-05-31 02:52:38 -0700

    Update numpyex.py

--- numpyex.py
+++ numpyex.py
@@ -3,3 +3,10 @@ new_arr = np.array([1, 2, 3, 4, 5, 6])
 print(new_arr[1:4])
 print(new_arr[3:])
 print(new_arr[:3])
+print(new_arr[-4: -1])
+
+print(new_arr[1:4:2])
+
+# Slice 2-d
+new_arr = np.array([[1, 2, 3, 4, 5], [10, 20, 30, 40, 50]])
+print(new_arr[1, 1:4])
