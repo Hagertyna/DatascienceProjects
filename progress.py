b97b8ed3da73666ff09a5f03ecb0a78939b143fe
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-05 06:57:59 -0700

    Update progress.py

--- progress.py
+++ progress.py
@@ -2,9 +2,9 @@
 import matplotlib.pyplot as plt
 
 left_coordinates=[1,2,3,4,5]
-heights=[5,60,50,75,100]
+percent=[5,60,50,75,100]
 bar_labels=['Deployment','Coding','Design ','Analysis','Documentation']
-plt.bar(left_coordinates,heights,tick_label=bar_labels,width=0.8,color=['blue','Brown'])
+plt.bar(left_coordinates,percent,tick_label=bar_labels,width=0.8,color=['blue','Brown'])
 plt.xlabel('Tasks Progress ')
 plt.ylabel('Percentage Done')
 plt.title("Medaresha Project Progress")
